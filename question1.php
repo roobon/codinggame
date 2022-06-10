abafb95f8072275efb6e015746993f6b42675141
Syed Ziaul Habib <roobon@gmail.com>
2022-06-10 23:38:14 +0600

    Question1 with header function completed

--- question1.php
+++ question1.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	
-</body>
+<html>
+<?php
+header('Location: http://www.prothomalo.com/');
+?>
 </html>
+This code produces an error. What can be done to correct it?
+
+<!-- Options
+a) Call function footer() before the end of the script
+b) Move the html tag after the call to header() //############### Correct Answer ###############
+c) Replace http://www.example.com with a valid server URL 
+-->
+
+<!-- 
+Reference:
+https://www.php.net/manual/en/function.header.php
+https://www.w3schools.com/php/func_network_header.asp 
+-->
